4567cbe4eac63de7aeeefae71295cb22591a1417
stcdojo_GIT <stcdojo@gmail.com>
2026-05-30 12:12:30 -0400

    crouching made, started on inventory
    yeah

---
+++ Brian-Black-Belt-Project/Assets/Inventory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Item> itemList;
+
+    public Inventory()
+    {
+        itemList = new List<Item>();
+
+        Debug.Log("Yes");
+    }
+
+}
---
+++ Brian-Black-Belt-Project/Assets/Item.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum ItemType
+    {
+        Stone,
+        Metal,
+        Wood,
+    }
+
+    public ItemType itemType;
+    public int amount;
+
+}
